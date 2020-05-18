445083ab946a3d8b0fdc95a195ea885666129269
Bappi <bappi.cs@gmail.com>
2020-05-18 16:59:53 +0600

    Initial files

---
+++ footer.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package bappi
+ */
+
+?>
+</div> <!-- row -->
+</div><!-- #content -->
+
+<footer id="colophon" class="site-footer">
+	<div class="row">
+		<div class="site-info">
+			<a href="<?php echo esc_url(__('https://wordpress.org/', 'bappi')); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf(esc_html__('Proudly powered by %s', 'bappi'), 'WordPress');
+				?>
+			</a>
+			<span class="sep"> | </span>
+			<?php
+			/* translators: 1: Theme name, 2: Theme author. */
+			printf(esc_html__('Theme: %1$s by %2$s.', 'bappi'), 'bappi', '<a href="http://bappi.blog/">&nbsp; Bappi </a>');
+			?>
+		</div><!-- .site-info -->
+	</div>
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
